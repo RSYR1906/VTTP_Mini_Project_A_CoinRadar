94cb6b7c3a0059ec0c1ece1bd93a1217d39cd88a
RSYR1906 <rsim9977@gmail.com>
2024-12-29 18:38:57 +0800

    Updates

--- src/main/java/com/nus/iss/sg/Mini_Project_A/config/WebConfig.java
+++ src/main/java/com/nus/iss/sg/Mini_Project_A/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.nus.iss.sg.Mini_Project_A.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("https://coinradar.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(3600)
+                .resourceChain(true);
     }
 }
