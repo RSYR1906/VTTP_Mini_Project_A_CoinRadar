914bd58a223fb3634c7166f4a2915631acb4c536
RSYR1906 <rsim9977@gmail.com>
2024-12-29 18:15:28 +0800

    Updates

--- src/main/java/com/nus/iss/sg/Mini_Project_A/constants/Url.java
+++ src/main/java/com/nus/iss/sg/Mini_Project_A/constants/Url.java
@@ -4,7 +4,7 @@ public class Url {
 
     public static final String newsUrl = "https://data-api.cryptocompare.com/news/v1/article/list?lang=EN";
 
-    public static final String cryptoListUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1";
+    public static final String cryptoListUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1";
 
     public static final String CRYPTOCOMPARE_NEWS_URL = "https://data-api.cryptocompare.com/news/v1/article/list";
 
